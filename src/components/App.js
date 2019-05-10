2a8ef8fd833f3eeebdf3ab3a57c218a74ac7b64d
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-05-10 13:44:35 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>PROJEKT</h1>
+      </>
+    )
+  }
+}
+export default App;
