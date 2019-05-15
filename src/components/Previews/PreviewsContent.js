9ccb7dc446e8b0854d031e76ba3202c6c6d27d34
MorozMichal <moroz_michal@o2.pl>
2019-05-15 21:47:48 +0200

    && in PreviewsContent

--- src/components/Previews/PreviewsContent.js
+++ src/components/Previews/PreviewsContent.js
@@ -8,15 +8,15 @@ const PreviewsContent = (props) => {
             <p>{run.city}</p>
             <p>{run.date.substring(8, 10)}.{run.date.substring(5, 7)}.{run.date.substring(0, 4)}</p> {/* zamiana formatu daty z tej z JSON 2019-02-12 na 12.02.2019 */}
             <p>{run.description}</p>
-            <p><a href={`${run.webOrganizer}`} rel="noopener">Strona organizatora</a></p>
-            <p><a href={`${run.regulations}`} rel="noopener">Regulamin</a></p>
-            <p><a href={`${run.records}`} rel="noopener">Zapisy</a></p>
-            <p>Termin: {run.fullDate}</p>
-            <p>Miejsce: {run.place}</p>
-            <p>Dystans: {run.distance}</p>
-            <p>limit uczestników: {run.limit}</p>
-            <p>Trasa: {run.route}</p>
-            <p>Opłata startowa: {run.payment}</p>
+            {run.webOrganizer && <p><a href={`${run.webOrganizer}`} rel="noopener">Strona organizatora</a></p>}
+            {run.regulations && <p><a href={`${run.regulations}`} rel="noopener">Regulamin</a></p>}
+            {run.records && <p><a href={`${run.records}`} rel="noopener">Zapisy</a></p>}
+            {run.fullDate && <p>Termin: {run.fullDate}</p>}
+            {run.place && <p>Miejsce: {run.place}</p>}
+            {run.distance && <p>Dystans: {run.distance}</p>}
+            {run.limit && <p>limit uczestników: {run.limit}</p>}
+            {run.route && <p>Trasa: {run.route}</p>}
+            {run.payment && <p>Opłata startowa: {run.payment}</p>}
         </div>
     ))
     return (
