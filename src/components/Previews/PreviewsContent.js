8fe27335718941209da81aa5484c43169840dfb1
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-05-16 12:43:08 +0200

    styles PreviewsContent

--- src/components/Previews/PreviewsContent.js
+++ src/components/Previews/PreviewsContent.js
@@ -11,12 +11,34 @@ const PreviewsContent = (props) => {
             <p><a href={`${run.webOrganizer}`} rel="noopener">Strona organizatora</a></p>
             <p><a href={`${run.regulations}`} rel="noopener">Regulamin</a></p>
             <p><a href={`${run.records}`} rel="noopener">Zapisy</a></p>
-            <p>Termin: {run.fullDate}</p>
-            <p>Miejsce: {run.place}</p>
-            <p>Dystans: {run.distance}</p>
-            <p>limit uczestników: {run.limit}</p>
-            <p>Trasa: {run.route}</p>
-            <p>Opłata startowa: {run.payment}</p>
+
+            <div className="preview-content-wraper-item">
+                <div>termin:</div>
+                <div>{run.fullDate}</div>
+            </div>
+            <div className="preview-content-wraper-item">
+                <div>miejsce:</div>
+                <div>{run.place}</div>
+            </div>
+            <div className="preview-content-wraper-item">
+                <div>dystans:</div>
+                <div>{run.distance}</div>
+            </div>
+            <div className="preview-content-wraper-item">
+                <div>limit:</div>
+                <div>{run.limit}</div>
+            </div>
+            <div className="preview-content-wraper-item">
+                <div>trasa:</div>
+                <div>{run.route}</div>
+            </div>
+            <div className="preview-content-wraper-item">
+                <div>opłata:</div>
+                <div>
+                    <p>{run.payment.payment1}</p>
+                    <p>{run.payment.payment2}</p>
+                </div>
+            </div>
         </div>
     ))
     return (
