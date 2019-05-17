e25f6b61bee289c3aac8e20e654e909b25896718
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-05-17 09:31:13 +0200

    sort date & show first 16 racing PreviewsRacing

--- src/components/Previews/PreviewsRacing.js
+++ src/components/Previews/PreviewsRacing.js
@@ -13,6 +13,15 @@ const PreviewsRacing = (props) => {
     //w JSON data format rrrr-mm-dd
     //zamienić zeby pkazywało sie od dnia na stronie
     const racingDate = props.racing.filter(run => Date.parse(run.date) > aktualnaData);
+    console.log(racingDate)
+
+    //posortowanie tablicy według daty
+    racingDate.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+    console.log(racingDate);
+
+    const racingDateShow = racingDate.slice(0, 16)
+    console.log(racingDateShow);
+
 
     // const aa = racingDate.map(run => (
     //     `${run.date.substring(8, 10)}.${run.date.substring(5, 7)}.${run.date.substring(0, 4)}`
@@ -20,7 +29,7 @@ const PreviewsRacing = (props) => {
 
     // console.log(aa)
 
-    const racing = racingDate.map(run => (
+    const racing = racingDateShow.map(run => (
         <div key={run.id} className="preview" onClick={() => props.click(run.id)}>
             <p>{run.city}</p>
             <p>{run.date.substring(8, 10)}.{run.date.substring(5, 7)}.{run.date.substring(0, 4)}</p> {/* zamiana formatu daty z tej z JSON 2019-02-12 na 12.02.2019 */}
