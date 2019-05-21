a3d2cdfb1c1210930de48b6d2615f8c31828e6b8
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-05-21 15:02:25 +0200

    text componentDid

--- src/components/Previews/Previews.js
+++ src/components/Previews/Previews.js
@@ -4,7 +4,7 @@ import PreviewsRacing from './PreviewsRacing'
 import PreviewsArrow from './PreviewsArrow'
 import PreviewsSearch from './PreviewsSearch';
 
-
+// const dateNow = new Date().getFullYear().toString()
 class Previews extends Component {
     state = {
         racing: [],
@@ -13,7 +13,7 @@ class Previews extends Component {
         classIcon: "fa-angle-left",
         classVisible: "",
         month: "0",
-        year: "",
+        year: "00-2019",
         region: "all",
         // disabled: false
 
@@ -50,88 +50,240 @@ class Previews extends Component {
         })
     }
 
-
     handleMonth = (e) => {
         this.setState({
             [e.target.name]: e.target.value,
         });
 
-        console.log(e.target.name)
-
-
-        if (e.target.name === "month") {
-            const daataMin = `${this.state.year}-${e.target.value}-01`
-            const daataMax = `${this.state.year}-${e.target.value}-31`
-            const region = this.state.region
-            const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
-            console.log(aaa)
-            this.setState({
-                racingMonth: aaa,
-                // disabled: false
-            })
-
-        }
-        if (e.target.name === "year") {
-            const daataMin = `${e.target.value}-${this.state.month}-01`
-            const daataMax = `${e.target.value}-${this.state.month}-31`
-            const region = this.state.region
-            const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
-            this.setState({
-                racingMonth: aaa
-            })
-
-        }
-
-
-        if (e.target.name === "region") {
-            const daataMin = `${this.state.year}-${this.state.month}-01`
-            const daataMax = `${this.state.year}-${this.state.month}-31`
-            const region = e.target.value
-            const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
-            this.setState({
-                racingMonth: aaa
-            })
-
-        }
-
-
-        if (e.target.name === "region" && e.target.value === "all") {
-            const daataMin = `${this.state.year}-${this.state.month}-01`
-            const daataMax = `${this.state.year}-${this.state.month}-31`
-            const region = e.target.value
-            const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region !== region)
-            this.setState({
-                racingMonth: aaa
-            })
-
-        }
-
-
-        if (this.state.month === "0" && e.target.value !== "all") {
-            const region = e.target.value
-            const aaa = this.state.racing.filter(array => array && array.region === region)
-            this.setState({
-                racingMonth: aaa,
-                // disabled: false
-            })
-        }
-        if (e.target.value === "0" && this.state.region !== "all") {
-            const region = this.state.region
-            const aaa = this.state.racing.filter(array => array && array.region === region)
-            this.setState({
-                racingMonth: aaa,
-                // disabled: false
-            })
-        }
-
-        if (e.target.value === "0") {
-            this.setState({
-                racingMonth: this.state.racing,
-                // disabled: true,
-                year: new Date().getFullYear()
-            })
-        }
     }
+
+
+
+    componentDidUpdate() {
+        const { region } = this.state
+        const aaa = this.state.racing.filter(array => array.region === region.slice(3))
+        if (region === "00-dolnośląskie" || region === "00-kujawsko-pomorskie" || region === "00-lubelskie" || region === "00-lubuski" || region === "00-łódzkie" || region === "00-małopolskie" || region === "00-mazowieckie" || region === "00-opolskie" || region === "00-podkarpackie" || region === "00-podlaskie" || region === "00-pomorskie" || region === "00-śląskie" || region === "00-świętokrzyskie" || region === "00-warmińsko-mazurskie" || region === "00-wielkopolskie" || region === "00-zachodniopomorskie") {
+            this.setState({
+                region: region.slice(3),
+                racingMonth: aaa
+            })
+        }
+        const { year, month } = this.state
+        const daataMin = year.length > 11 ? `${year.slice(3)}-${month.slice(3)}-01` : `${year}-${this.state.month}-01`;
+        const daataMax = year.length > 11 ? `${year.slice(3)}-${month.slice(3)}-31` : `${year}-${month}-31`;
+        console.log("to warunek min: " + daataMin);
+        console.log("to warunek mix: " + daataMax);
+        console.log("state - " + year);
+        console.log("state - " + month);
+        const bbbb = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax)
+        if (month === "00-06" && (year === "00-2019" || year === "00-2020")) {
+            this.setState({
+                racingMonth: bbbb,
+                year: year.slice(3),
+                month: month.slice(3),
+
+            })
+
+        }
+        console.log(bbbb)
+
+
+    }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     // console.log(prevState.year)
+    //     return {
+    //         prevState
+    //     }
+
+    // }
+
+    // componentDidUpdate(newProps, prevState, year) {
+    //     console.log(year)
+    //     // console.log(newState.month)
+    //     // console.log(newState.region)
+
+    // }
+
+
+    // handleMonth = (e) => {
+    //     const { year, month, region } = this.state;
+
+    // const { year, month, region } = e.target.value
+
+    // this.setState({
+    //     [e.target.name]: e.target.value,
+
+    // });
+
+
+    // if (month === "0" && year === "2019" && region === "all") {
+    //     console.log("ok")
+    // }
+
+    // if (e.target.value === "all") {
+    //     console.log("ok")
+    //     console.log(this.state.region)
+    // }
+
+    // if (e.target.name === "month") {
+    //     const daataMin = `${this.state.year}-${e.target.value}-01`
+    //     const daataMax = `${this.state.year}-${e.target.value}-31`
+    //     const region = this.state.region
+    //     const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //     console.log(aaa)
+    //     this.setState({
+    //         racingMonth: aaa,
+    //         // disabled: false
+    //     })
+
+    // }
+
+    // if (e.target.name === "year") {
+    //     const daataMin = `${e.target.value}-${this.state.month}-01`
+    //     const daataMax = `${e.target.value}-${this.state.month}-31`
+    //     const region = this.state.region
+    //     const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //     this.setState({
+    //         racingMonth: aaa
+    //     })
+
+    // }
+
+
+    // if (e.target.name === "region") {
+    //     const daataMin = `${this.state.year}-${this.state.month}-01`
+    //     const daataMax = `${this.state.year}-${this.state.month}-31`
+    //     const region = e.target.value
+    //     const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //     this.setState({
+    //         racingMonth: aaa
+    //     })
+
+    // }
+
+
+    // if (e.target.name === "region" && e.target.value === "all") {
+    //     const daataMin = `${this.state.year}-${this.state.month}-01`
+    //     const daataMax = `${this.state.year}-${this.state.month}-31`
+    //     const region = e.target.value
+    //     const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region !== region)
+    //     this.setState({
+    //         racingMonth: aaa
+    //     })
+
+    // }
+
+
+    // if (this.state.month === "0" && e.target.value !== "all") {
+    //     const region = e.target.value
+    //     const aaa = this.state.racing.filter(array => array && array.region === region)
+    //     this.setState({
+    //         racingMonth: aaa,
+    //         // disabled: false
+    //     })
+    // }
+
+    // if (e.target.value === "0" && this.state.region !== "all") {
+    //     const region = this.state.region
+    //     const aaa = this.state.racing.filter(array => array && array.region === region)
+    //     this.setState({
+    //         racingMonth: aaa,
+    //         // disabled: false
+    //     })
+    // }
+
+    // if (e.target.value === "0") {
+    //     this.setState({
+    //         racingMonth: this.state.racing,
+    //         // disabled: true,
+    //         year: new Date().getFullYear()
+    //     })
+    // }
+
+
+    // }
+
+
+
+    // handleMonth = (e) => {
+    //     this.setState({
+    //         [e.target.name]: e.target.value,
+    //     });
+
+    //     if (e.target.name === "month") {
+    //         const daataMin = `${this.state.year}-${e.target.value}-01`
+    //         const daataMax = `${this.state.year}-${e.target.value}-31`
+    //         const region = this.state.region
+    //         const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //         console.log(aaa)
+    //         this.setState({
+    //             racingMonth: aaa,
+    //             // disabled: false
+    //         })
+
+    //     }
+    //     if (e.target.name === "year") {
+    //         const daataMin = `${e.target.value}-${this.state.month}-01`
+    //         const daataMax = `${e.target.value}-${this.state.month}-31`
+    //         const region = this.state.region
+    //         const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //         this.setState({
+    //             racingMonth: aaa
+    //         })
+
+    //     }
+
+
+    //     if (e.target.name === "region") {
+    //         const daataMin = `${this.state.year}-${this.state.month}-01`
+    //         const daataMax = `${this.state.year}-${this.state.month}-31`
+    //         const region = e.target.value
+    //         const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region === region)
+    //         this.setState({
+    //             racingMonth: aaa
+    //         })
+
+    //     }
+
+
+    //     if (e.target.name === "region" && e.target.value === "all") {
+    //         const daataMin = `${this.state.year}-${this.state.month}-01`
+    //         const daataMax = `${this.state.year}-${this.state.month}-31`
+    //         const region = e.target.value
+    //         const aaa = this.state.racing.filter(array => array.date >= daataMin && array.date <= daataMax && array.region !== region)
+    //         this.setState({
+    //             racingMonth: aaa
+    //         })
+
+    //     }
+
+
+    //     if (this.state.month === "0" && e.target.value !== "all") {
+    //         const region = e.target.value
+    //         const aaa = this.state.racing.filter(array => array && array.region === region)
+    //         this.setState({
+    //             racingMonth: aaa,
+    //             // disabled: false
+    //         })
+    //     }
+    //     if (e.target.value === "0" && this.state.region !== "all") {
+    //         const region = this.state.region
+    //         const aaa = this.state.racing.filter(array => array && array.region === region)
+    //         this.setState({
+    //             racingMonth: aaa,
+    //             // disabled: false
+    //         })
+    //     }
+
+    //     if (e.target.value === "0") {
+    //         this.setState({
+    //             racingMonth: this.state.racing,
+    //             // disabled: true,
+    //             year: new Date().getFullYear()
+    //         })
+    //     }
+    // }
 
 
     handleIcon = () => {
--- src/components/Previews/PreviewsSearch.js
+++ src/components/Previews/PreviewsSearch.js
@@ -6,41 +6,42 @@ const PreviewsSearch = (props) => {
         <div className="section-previews-search">
             <select name="month" value={props.month} onChange={props.inputMonth}>
                 <option value="0">Najbliższe</option>
-                <option value="01">Styczeń</option>
-                <option value="02">Luty</option>
-                <option value="03">Marzec</option>
-                <option value="04">Kwiecień</option>
-                <option value="05">Maj</option>
-                <option value="06">Czerwiec</option>
-                <option value="07">Lipiec</option>
-                <option value="08">Sierpień</option>
-                <option value="09">Wrzesień</option>
-                <option value="10">Październik</option>
-                <option value="11">Listopad</option>
-                <option value="12">Grudzień</option>
+                <option value="00-01">Styczeń</option>
+                <option value="00-02">Luty</option>
+                <option value="00-03">Marzec</option>
+                <option value="00-04">Kwiecień</option>
+                <option value="00-05">Maj</option>
+                <option value="00-06">Czerwiec</option>
+                <option value="00-07">Lipiec</option>
+                <option value="00-08">Sierpień</option>
+                <option value="00-09">Wrzesień</option>
+                <option value="00-10">Październik</option>
+                <option value="00-11">Listopad</option>
+                <option value="00-12">Grudzień</option>
             </select>
-            <select name="year" placeholder="" value={props.year} onChange={props.inputMonth} disabled={props.disabled}>
-                <option value={yearNow}>{yearNow}</option>
-                <option value={yearNow + 1}>{yearNow + 1}</option>
+            <select name="year" value={props.year} onChange={props.inputMonth} disabled={props.disabled}>
+                <option value={"all"}>all</option>
+                <option value={"2019"}>{yearNow}</option>
+                <option value={"2020"}>{yearNow + 1}</option>
             </select>
             <select name="region" value={props.region} onChange={props.inputMonth}>
                 <option value="all">Wszystkie</option>
-                <option value="dolnośląskie">Dolnośląskie</option>
-                <option value="kujawsko-Pomorskie">Kujawsko-Pomorskie</option>
-                <option value="lubelskie">Lubelskie</option>
-                <option value="lubuskie">Lubuskie</option>
-                <option value="łódzkie">Łódzkie</option>
-                <option value="małopolskie">Małopolskie</option>
-                <option value="mazowieckie">Mazowieckie</option>
-                <option value="opolskie">Opolskie</option>
-                <option value="podkarpackie">Podkarpackie</option>
-                <option value="podlaskie">Podlaskie</option>
-                <option value="pomorskie">Pomorskie</option>
-                <option value="śląskie">Śląskie</option>
-                <option value="świętorzyskie">Świętorzyskie</option>
-                <option value="warmińsko-Mazurskie">Warmińsko-Mazurskie</option>
-                <option value="wielkopolskie">Wielkopolskie</option>
-                <option value="zachodniopomorskie">Zachodniopomorskie</option>
+                <option value="00-dolnośląskie">Dolnośląskie</option>
+                <option value="00-kujawsko-pomorskie">Kujawsko-Pomorskie</option>
+                <option value="00-lubelskie">Lubelskie</option>
+                <option value="00-lubuskie">Lubuskie</option>
+                <option value="00-łódzkie">Łódzkie</option>
+                <option value="00-małopolskie">Małopolskie</option>
+                <option value="00-mazowieckie">Mazowieckie</option>
+                <option value="00-opolskie">Opolskie</option>
+                <option value="00-podkarpackie">Podkarpackie</option>
+                <option value="00-podlaskie">Podlaskie</option>
+                <option value="00-pomorskie">Pomorskie</option>
+                <option value="00-śląskie">Śląskie</option>
+                <option value="00-świętorzyskie">Świętorzyskie</option>
+                <option value="00-warmińsko-mazurskie">Warmińsko-Mazurskie</option>
+                <option value="00-wielkopolskie">Wielkopolskie</option>
+                <option value="00-zachodniopomorskie">Zachodniopomorskie</option>
             </select>
         </div>
     )
